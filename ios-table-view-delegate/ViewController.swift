d6a9d92235ec41ad6958f5c626b546875d5f6db3
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-03-14 00:05:59 -0700

    Initial Commit

---
+++ ios-table-view-delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-table-view-delegate
+//
+//  Created by jianyuan chen on 2019-03-14.
+//  Copyright © 2019 Jianyuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
